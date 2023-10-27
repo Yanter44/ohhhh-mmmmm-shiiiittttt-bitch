3919d30c77ce1479c23f33c72038443a0cc6cd43
User <User@STUDENT3-A308>
2023-10-27 10:05:30 +0300

    Добавьте файлы проекта.

---
+++ ohhhh mmmmm shiiiittttt bitch/AtributeName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ohhhh_mmmmm_shiiiittttt_bitch
+{
+    internal class AtributeName : Attribute
+    {
+
+    }
+}
---
+++ ohhhh mmmmm shiiiittttt bitch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ohhhh_mmmmm_shiiiittttt_bitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
