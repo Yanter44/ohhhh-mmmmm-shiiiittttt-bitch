43a8e354a50e75f9c67ba6ff6ece2ca82b1c813a
User <User@STUDENT3-A308>
2023-10-31 08:32:30 +0300

    -coomitthithname

--- ohhhh mmmmm shiiiittttt bitch/Program.cs
+++ ohhhh mmmmm shiiiittttt bitch/Program.cs
@@ -10,16 +10,26 @@ namespace ohhhh_mmmmm_shiiiittttt_bitch
     {
         static void Main(string[] args)
         {
-            Human h = new Human();
-            h.go();
-            h.Daun();
+            const int r = 1;
+            string x, y;
+            Console.Write("Enter x: ");
+            x = Console.ReadLine();
+            Console.Write("Enter y: ");
+            y = Console.ReadLine();
+
+            double s = Math.Sqrt(Math.Pow(double.Parse(x), 2) + Math.Pow(double.Parse(y), 2));
+
+            Console.WriteLine($"Prinadleshit: {s == r}");
         }
-      
+
     }
 
     class Human
     {
-       public void go()
+
+        public bool IsKukold { get; set; } = true;
+        public string Name { get; set; }    
+        public void Go()
         {
             Console.WriteLine("O");
         }
@@ -30,11 +40,12 @@ namespace ohhhh_mmmmm_shiiiittttt_bitch
 
         public void SuckDick(int howDeep)
         {
-            if(howDeep > 15)
+            if (howDeep > 15)
             {
                 Console.WriteLine("Jopa lopnula");
             }
             Console.WriteLine("MMMM yammy dick");
         }
+     
     }
 }
